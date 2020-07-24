33ac73215a1c8e3708b1effb32b1367da81d7fea
Andrew Chau <andrewchau333@gmail.com>
2020-07-23 22:41:00 -0700

    updating cors package

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Environment setup
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 4000;
 
 const app = express();
-// 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+app.use(cors());
 // parse application/json and application/x-www-form-urlencoded
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
