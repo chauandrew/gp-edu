3a85b68ad18b53ea9a453616c2fdb9df2d398167
Andrew Chau <andrewchau333@gmail.com>
2020-07-27 16:26:58 -0700

    Updated header

---
+++ client/src/assets/icons/index.jsx
@@ -0,0 +1,9 @@
+// get bootstrap icons from https://icons.getbootstrap.com/
+import HouseDoor from './house-door.svg'
+import Calendar from './calendar.svg'
+import PersonCircle from './person-circle.svg'
+import Chat from './chat.svg'
+
+export {
+    HouseDoor, Calendar, PersonCircle, Chat
+}
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,37 +1,65 @@
-import React, {useContext} from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React, { useContext, useState, useEffect } from 'react';
+import { Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
 
 import { AuthContext } from "../../auth/Auth";
+import * as icons from "../../assets/icons";
 import logo from '../../assets/logo.png';
 import './Header.css';
 import db from '../../firebase';
 
 
 const Header = () => {
-  const { currentUser, pending } = useContext(AuthContext);
+  const { currentUser, pending } = useContext(AuthContext)
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth < 992 ? true : false
+  )
+  // event handler on window resize
+  useEffect(() => {
+    const widthHandler = () => {
+      setIsMobile(window.innerWidth < 992 ? true : false)
+    }
+    window.addEventListener('resize', widthHandler)
+    return () => { window.removeEventListener('resize', widthHandler) }
+  })
 
+  // don't display profile icon in mobile view
+  if (isMobile) {
+    var profileElement =
+      <>
+        <Nav.Link href='/profile' className='text-secondary mt-auto mb-auto'>Profile</Nav.Link>
+        <Nav.Link href='/login' className='text-secondary mt-auto mb-auto'
+          onClick={() => { db.auth().signOut() }}>Logout</Nav.Link>
+      </>
+  } else {
+    profileElement =
+      <>
+        <NavDropdown id='profile-dropdown' className="dropdown-menu-right"
+          title={<img src={icons.PersonCircle} alt='logo' width='35' height='35'></img>}>
+          <NavDropdown.Item href='/profile' className='text-secondary mt-auto mb-auto'>Profile</NavDropdown.Item>
+          <NavDropdown.Item href='/login' className='text-secondary mt-auto mb-auto'
+            onClick={() => { db.auth().signOut() }}>Logout</NavDropdown.Item>
+        </NavDropdown>
+      </>
+  }
+
+  // Only display header when user is signed in
   if (!pending && currentUser) {
     return (
-      <Navbar collapseOnSelect fixed='top' expand='lg' bg='dark' variant='dark'>
+      <Navbar collapseOnSelect fixed='top' expand='lg' bg='light' variant='light'>
         <Navbar.Brand href='/'>
           <img src={logo} alt='logo' width='50' height='50'></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-          <Nav className="mr-auto">
-            <Nav.Link href='/' className='text-secondary'>Home</Nav.Link>
-            <Nav.Link href='/browse' className='text-secondary'>Browse</Nav.Link>
-            <Nav.Link href='/courses' className='text-secondary'>My Courses</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href='/profile' className='text-secondary'>Profile</Nav.Link>
-            <Nav.Link className='text-secondary' 
-              onClick={ () => { db.auth().signOut()} }>Logout</Nav.Link>
+          <Nav className="ml-auto">
+            <Nav.Link href='/' className='text-secondary mt-auto mb-auto'>Home</Nav.Link>
+            <Nav.Link href='/browse' className='text-secondary mt-auto mb-auto'>Browse</Nav.Link>
+            <Nav.Link href='/courses' className='text-secondary mt-auto mb-auto'>My Courses</Nav.Link>
+            {profileElement}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    );
+    )
   } else {
     return null
   }
