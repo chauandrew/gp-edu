490146e1f3e00d13acc229d8a860e74a2031e93e
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-27 16:31:22 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
             </style>
-            <h1 style = {{fontSize: "30px", position: 'absolute', left: 275, top: 150, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h1 style = {{fontSize: "35px", position: 'absolute', left: 275, top: 150, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 ABOUT US
             </h1>
-            <h2 style = {{fontSize: "20px", position: 'absolute', left: 371.93, top: 274.62, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h2 style = {{fontSize: "18px", position: 'absolute', left: 320, top: 200, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 WHO WE ARE
             </h2>
-            <p style = {{fontSize: "16px", position: "absolute", left: 370.38, top: 330.78, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 315, top: 230, fontFamily: "Helvetica Neue"}}>
                 We are a cool group of people called Area Youth Ministries. We care about educational equality and growth for are youth, especially in underserved areas.
             </p>
         </div>
