a0e5d00599d83a7e8ca5045f329508f9587ffd81
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-27 16:58:15 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -10,16 +10,16 @@ const About = () => {
             <h1 style = {{fontSize: "35px", position: 'absolute', left: 275, top: 150, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 ABOUT US
             </h1>
-            <h2 style = {{fontSize: "18px", position: 'absolute', left: 320, top: 225, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 225, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 WHO WE ARE
             </h2>
-            <p style = {{fontSize: "12px", position: "absolute", left: 317, top: 265, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 327, top: 260, fontFamily: "Helvetica Neue"}}>
                 We are a cool group of people called <br /> Area Youth Ministries. We care about <br /> educational equality and growth for are <br /> youth, especially in underserved areas.
             </p>
-            <h2 style = {{fontSize: "18px", position: 'absolute', left: 320, top: 305, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 350, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 OUR MISSION
             </h2>
-            <p style = {{fontSize: "12px", position: "absolute", left: 317, top: 345, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 327, top: 385, fontFamily: "Helvetica Neue"}}>
                 Our mission is to inspire and educate <br /> the next generation of young learners <br /> and help them realize their potential <br /> to change the world.
             </p>
         </div>
