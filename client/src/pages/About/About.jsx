a5d21e4f25c758a548a28f6f6b08562deec9fa96
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-27 16:26:01 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -3,10 +3,10 @@ import './About.css'
 
 const About = () => {
     return (
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
-        </style>
         <div className="page-content">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
+            </style>
             <h1 style = {{fontSize: "40px", position: 'absolute', left: 299.82, top: 167.06, fontWeight: "bold", fontFamily: "DM Sans"}}>
                 ABOUT US
             </h1>
