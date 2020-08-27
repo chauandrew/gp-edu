b3d263623fcf5ee22ea3edeccf917eba2b2f6794
lausunny <38926061+stl122@users.noreply.github.com>
2020-08-27 11:10:13 -1000

    Added button links and course names now change color
    Buttons currently don't do anything, but they change color on hover.
    Course names change color based on the subject.

--- client/src/pages/Subject/Subject.jsx
+++ client/src/pages/Subject/Subject.jsx
@@ -7,23 +7,79 @@ const Subject = () => {
     const { subjectId } = useParams()
     const [courses, setCourses] = useState([])
 
-    var courseList = ""
+    if (subjectId) {
+        var color
+        if (subjectId == "math") {
+            color = "red"
+        }
+        if (subjectId == "science") {
+            color = "orange"
+        }
+        if (subjectId == "humanities") {
+            color = "blue"
+        }
+        if (subjectId == "life skills") {
+            color = "green"
+        }
+        if (subjectId == "computer science") {
+            color = "purple"
+        }
+    }
+
+    if (color) {
+        if (color == "red") {
+            var txtColor = '#BA4040'
+            var btnColor = '#D66E6E'
+            var hvrColor = '#BA0000'
+        }
+        if (color == "orange") {
+            var txtColor = '#CB815C'
+            var btnColor = '#DBA78D'
+            var hvrColor = '#'
+        }
+        if (color == "blue") {
+            var txtColor = '#6391C0'
+            var btnColor = '#92B2D3'
+            var hvrColor = '#'
+        }
+        if (color == "green") {
+            var txtColor = '#6EA1A1'
+            var btnColor = '#92B8B9'
+            var hvrColor = '#'
+        }
+        if (color == "purple") {
+            var txtColor = '#7A7DCA'
+            var btnColor = '#A2A4DA'
+            var hvrColor = '#'
+        }
+    }
+
+    var courseList = []
     if (courses) {
         for (let i in courses) {
-            courseList += `${courses[i].course_name}, `
+            let element = 
+                <div class="topicList">
+                    <h4 class="courseName" style={{color:txtColor}}>{courses[i].course_name}</h4>
+                    <div class="btn-group">
+                        <a href="#" class="button">Topic 1</a>
+                        <a href="#" class="button">Topic 2</a>
+                    </div>
+                </div>
+            courseList.push(element)
         }
     }
 
     useEffect(() => {
         api.getCoursesBySubject(subjectId).then(c => {
             setCourses(c.data)
+            console.log(c.data)
         })
     }, [subjectId])
 
     return (
-        <div className="page-content">
-            <h1>Subject: {subjectId}</h1>
-            <p>{courseList}</p>
+        <div class="page-content" className="page-content">
+            <h1>{subjectId}</h1>
+            {courseList}
         </div>
     )
 }
