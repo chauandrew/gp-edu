8ff7c222ecc81baf83342d384661c4c45d968d64
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-27 15:50:51 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -8,6 +8,8 @@ const About = () => {
                 ABOUT US
             </h1>
             <h2 style = {{fontSize: "20px", position: 'absolute', left: 371.93, top: 274.62, fontWeight: "bold", fontFamily: "DM Sans"}}>
+                WHO WE ARE
+            </h2>
             <p style = {{fontSize: "16px", position: "absolute", left: 370.38, top: 330.78, fontFamily: "Helvetica Neue"}}>
                 We are a cool group of people called Area Youth Ministries. We care about educational equality and growth for are youth, especially in underserved areas.
             </p>
