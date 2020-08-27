568b7275bf1a200d342c93e9b09b5c8baee09f64
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-26 17:58:46 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -4,7 +4,7 @@ import './About.css'
 const About = () => {
     return (
         <div className="page-content">
-            <h1 style = {{fontSize: "40px", top: "200px", left:"200px", bottom: "auto", right: "auto", fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h1 style = {{fontSize: "40px", height: "200px", width: "200px", fontWeight: "bold", fontFamily: "DM Sans"}}>
                 ABOUT US
             </h1>
         </div>
