8095b5f4344ba5c146c8e3deaff132e179a9f3fe
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-27 17:08:53 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -10,22 +10,22 @@ const About = () => {
             <h1 style = {{fontSize: "35px", position: 'absolute', left: 275, top: 150, fontWeight: "bold", fontFamily: "DM Sans", letterSpacing: "0.05em"}}>
                 ABOUT US
             </h1>
-            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 225, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 225, fontWeight: "bold", fontFamily: "DM Sans", letterSpacing: "0.05em"}}>
                 WHO WE ARE
             </h2>
-            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 260, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 260, fontFamily: "Helvetica Neue", letterSpacing: "0.05em"}}>
                 We are a cool group of people called <br /> Area Youth Ministries. We care about <br /> educational equality and growth for are <br /> youth, especially in underserved areas.
             </p>
-            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 350, fontWeight: "bold", fontFamily: "DM Sans"}}>
+            <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 350, fontWeight: "bold", fontFamily: "DM Sans", letterSpacing: "0.05em"}}>
                 OUR MISSION
             </h2>
-            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 385, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 385, fontFamily: "Helvetica Neue", letterSpacing: "0.05em"}}>
                 Our mission is to inspire and educate <br /> the next generation of young learners <br /> and help them realize their potential <br /> to change the world.
             </p>
-             <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 475, fontWeight: "bold", fontFamily: "DM Sans"}}>
+             <h2 style = {{fontSize: "18px", position: 'absolute', left: 330, top: 475, fontWeight: "bold", fontFamily: "DM Sans", letterSpacing: "0.05em"}}>
                 INTERACTIVE EDUCATION
             </h2>
-            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 510, fontFamily: "Helvetica Neue"}}>
+            <p style = {{fontSize: "12px", position: "absolute", left: 328, top: 510, fontFamily: "Helvetica Neue", letterSpacing: "0.05em"}}>
                 This platform offers a wide variety of <br /> interactive courses in mathematics, <br /> science, humanities, programming, <br /> and life skills.
             </p>
         </div>
