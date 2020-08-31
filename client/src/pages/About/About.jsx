fd1e497b803a068bab643e59f37a570921d202cc
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-30 20:51:39 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
                 style = {{position: 'relative', left: 480, top: 370, height: '12%', width: '12%'}}
             />
             <img src={treesSmall} alt = "small trees"
-                style = {{position: 'relative', left: 780, top: 370, height: '12%', width: '12%'}}
+                style = {{position: 'relative', left: 880, top: 400, height: '9%', width: '9%'}}
             />
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
