bd705612b238965c43050c3fc1641b8482bb03e4
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-08-30 20:54:38 -0700

    Update About.jsx

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                 style = {{position: 'relative', left: 1000, top: 388, height: '9%', width: '9%'}}
             />
             <img src={ladyLaptop} alt = "lady laptop"
-                style = {{position: 'relative', left: 1000, top: 388, height: '9%', width: '9%'}}
+                style = {{position: 'relative', left: 700, top: 270, height: '15%', width: '15%'}}
             />
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
