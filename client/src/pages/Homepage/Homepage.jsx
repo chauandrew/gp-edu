885684f6c67aa6c09033fd28077ae70f570c4b26
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-09-02 17:52:32 -0700

    Update Homepage.jsx

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -4,7 +4,9 @@ import './Homepage.css';
 const Homepage = ({history}) => {
   return (
     <div className="page-content"> 
-      
+      <h1 className="header" style = {{left: 262, top: 130}}>
+        HI THERE. WELCOME!
+      </h1>
     </div>
   );
 }
