d75bb0ea8ff6bc7f7fd19872af82bf0494642b7d
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-09-02 18:11:27 -0700

    Update Homepage.jsx

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -4,10 +4,10 @@ import './Homepage.css';
 const Homepage = ({history}) => {
   return (
     <div className="page-content"> 
-      <h1 className="welcome" style = {{left: 100, top: 115}}>
+      <h1 className="welcome" style = {{left: 100, top: 135}}>
         HI THERE. WELCOME!
       </h1>
-      <h2 className= "click" style = {{left:100, top: 190}}>
+      <h2 className= "click" style = {{left:100, top: 210}}>
         Click on a subject to get started!
       </h2>
     </div>
