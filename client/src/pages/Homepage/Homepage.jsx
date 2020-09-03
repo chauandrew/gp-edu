712254135d2d2cb74e9bfdc22b2be2ca7aeff2ef
danielz117 <58540799+danielz117@users.noreply.github.com>
2020-09-02 18:38:50 -0700

    Update Homepage.jsx

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -7,10 +7,10 @@ import robot from '../../assets/Robot.png'
 const Homepage = ({history}) => {
   return (
     <div className="page-content"> 
-      <h1 className="welcome" style = {{left: 100, top: 105}}>
+      <h1 className="welcome" style = {{left: 100, top: 115}}>
         HI THERE. WELCOME!
       </h1>
-      <h2 className= "click" style = {{left:100, top: 160}}>
+      <h2 className= "click" style = {{left:100, top: 170}}>
         Click on a subject to get started!
       </h2>
       
